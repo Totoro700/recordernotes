7a1dee5b975456678ecd65249bfdc6c697f5cd2b
Totoro700 <ianfrancisko@gmail.com>
2024-07-27 20:40:44 -0500

    remove octave except for c

--- notestofingering.py
+++ notestofingering.py
@@ -1,15 +1,15 @@
 import time, os
 fingerings = {
-    'E4': '0 3 2',
-    'F#4': '0 3 1',
-    'G4': '0 3 0',
-    'A4': '0 2 0',
-    'D4': '0 3 3',
+    'E': '0 3 2',
+    'F#': '0 3 1',
+    'G': '0 3 0',
+    'A': '0 2 0',
+    'D': '0 3 3',
     'C4': '0 3 4',
-    'B4': '0 1 0',
+    'B': '0 1 0',
     'C5': '0 -1/2',
-    'D#4': '0 3 3.5',
-    'A#4': '0 1.5 0'
+    'D#': '0 3 3.5',
+    'A#': '0 1.5 0'
 }
 
 # Function to convert a list of notes to their fingerings
@@ -17,7 +17,7 @@ def convert_notes_to_fingerings(notes):
     return [fingerings[note] for note in notes if note in fingerings]
 
 # Example list of notes
-notes = input("Notes to convert: (format like this without quotation marks: \"E4 C5 D4 A#4 D#4\", see docs for list of notes available)" ).split(" ")
+notes = input("Notes to convert: (format like this without quotation marks: \"E C5 D A# D#\", see docs for list of notes available)" ).split(" ")
 
 # Convert the notes to fingerings
 fingerings_list = convert_notes_to_fingerings(notes)
